ddb4e3d65e7d49a9807c96ed494c67d6aa04b0a0
TEMPORAL2 <temporal2@itchii.lab>
2018-08-27 09:47:55 -0600

    v1

---
+++ main/Main.java
@@ -0,0 +1,18 @@
+/*
+    Primera práctica en Java. Definición de nomenclatura para prácticas
+    e introduccion al ambiente (IDE).
+*/
+package main;
+/**
+ * 
+ * @author Alfredo Antonio Martinez Armendariz 18550713
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //Primera línea de código: Hola mundo!!
+        System.out.println("Hola");
+        System.out.println("Hola mundo!!");//println --> imprime un mensaje
+    }
+    
+}
